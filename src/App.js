3f2cc71556f76f27e514b6eb59859cf02f9c147b
Avi Rana <itzavi1718@gmail.com>
2023-12-21 19:53:57 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
     <NavBar />
-    <main className="bg-white m-2.5 max-h-full shadow-[rgba(0,0,0,0.02)_0px_1px_3px_0px,rgba(27,31,35,0.15)_0px_0px_0px_1px] h-full p-4 rounded-[5px] md:mx-[20vw]">
+    <main className="bg-white m-3 max-h-full shadow-[rgba(0,0,0,0.02)_0px_1px_3px_0px,rgba(27,31,35,0.15)_0px_0px_0px_1px] h-full p-4 rounded-[5px] md:mx-[20vw]">
     <Routes>
       <Route path="/" element={<QuestionList/>} />
       <Route path="answers/:id" element={<QuestionDiv/>} />
