9ff89aa34e510f3310cbd86ebcae2684fd917a23
Avi Rana <itzavi1718@gmail.com>
2023-12-21 19:53:22 +0530

    Update Listitem.js

--- src/component/Listitem.js
+++ src/component/Listitem.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 function Listitem(props) {
     return (
-        <li className="list-none m-1.5 p-2 rounded shadow-[rgba(0,0,0,0.02)_0px_1px_3px_0px,rgba(27,31,35,0.15)_0px_0px_0px_1px] hover:underline">
+        <li className="list-none font-['Epilogue'] m-1.5 p-2 rounded shadow-[rgba(0,0,0,0.02)_0px_1px_3px_0px,rgba(27,31,35,0.15)_0px_0px_0px_1px] hover:underline">
          <Link to={props.url}>
         <span>{props.title}</span>
            <i class="fa-solid fa-up-right-from-square m-1.5 text-[#0D6EFD]"></i></Link>
