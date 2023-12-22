5ef66a073053d38dc406689a244bd840984b2de8
Avi Rana <itzavi1718@gmail.com>
2023-12-22 19:29:17 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
     <NavBar />
-    <main className="bg-white max-h-full min-h-screen m-3 md:!mx-[25vw] shadow-[rgba(0,0,0,0.02)_0px_1px_3px_0px,rgba(27,31,35,0.15)_0px_0px_0px_1px] h-full p-4 rounded-[5px]">
+    <main className="bg-white max-h-full m-3 md:!mx-[25vw] shadow-[rgba(0,0,0,0.02)_0px_1px_3px_0px,rgba(27,31,35,0.15)_0px_0px_0px_1px] p-4 rounded-[5px]">
     <Routes>
       <Route path="/" element={<QuestionList/>} />
       <Route path="answers/:id" element={<QuestionDiv/>} />
--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,6 +1,6 @@
 function Footer() {
     return (
-        <footer className="bg-white max-w-full p-3 shadow-[rgba(0,0,0,0.02)_0px_1px_3px_0px,rgba(27,31,35,0.15)_0px_0px_0px_1px] flex font-['Epilogue']">
+        <footer className="bg-[#F2F6FC] max-w-full p-3 shadow-[rgba(0,0,0,0.02)_0px_1px_3px_0px,rgba(27,31,35,0.15)_0px_0px_0px_1px] flex font-['Epilogue']">
             <div className="flex items-center">
             <img className="w-12 m-1" src="/logo.png" alt="logo"></img>
         <h3>Disha Learning</h3>
