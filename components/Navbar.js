b0f389842a8fc6bcdf6408026258eeece988214b
Avi Rana <itzavi1718@gmail.com>
2023-12-30 00:06:45 +0530

    broken code

--- components/Navbar.js
+++ components/Navbar.js
@@ -4,7 +4,7 @@ function NavBar() {
  return (
     <>
     <nav className="p-2.5 text-3xl font-medium">
-        <Link href="/" className="flex items-center" passHref>
+        <Link href="/" className="flex items-center">
         <img className="w-12 m-1" src="/logo.png" alt="logo"></img>
         <h3>Disha Learning</h3>
         </Link>
